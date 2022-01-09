c4120265e4474c084c674b8777c6c479f8a8db9a
rdrg0 <rodrigo.arriaran@gmail.com>
2022-01-09 14:58:40 -0500

    Add limit and offset for pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,14 +12,21 @@ class ProductsController < ApplicationController
     json_response(@product)
   end
 
-  private    
+  private
+    
+    MAX_LIMIT = 50
+
+    def limit
+      [params.fetch(:limit, MAX_LIMIT).to_i, MAX_LIMIT].min
+    end
+
     def set_products
-      if params[:q]      
+      if params[:q]
         partial_1 = Product.where('product.name LIKE :q', q: "%#{params[:q]}%")
-        partial_2 = Product.where(category: Category.where('category.name LIKE :q', q: "%#{params[:q]}%"))      
-        @products = partial_1.or(partial_2)
+        partial_2 = Product.where(category: Category.where('category.name LIKE :q', q: "%#{params[:q]}%"))
+        @products = partial_1.or(partial_2).limit(limit).offset(params[:offset])
       else
-        @products = Product.limit(30).offset(0)
+        @products = Product.limit(limit).offset(params[:offset])
       end
     end
 end
