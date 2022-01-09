e6560d4be5a7fcdcebea72359b089d7894576f2a
rdrg0 <rodrigo.arriaran@gmail.com>
2022-01-09 12:41:16 -0500

    Implement search for products by name and category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,9 @@
 class ProductsController < ApplicationController
-    
+    before_action :set_products, only: :index
 
   # GET /products
-  def index
-    @products = Product.all
-    json_response(@products)
+  def index    
+    json_response(@products)    
   end
 
   # GET /products/1
@@ -13,19 +12,14 @@ class ProductsController < ApplicationController
     json_response(@product)
   end
 
-
-
- 
-
- 
-
-
-
-
-  private
-
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:name, :url_image, :price, :discount, :category)
+  private    
+    def set_products
+      if params[:q]      
+        partial_1 = Product.where('product.name LIKE :q', q: "%#{params[:q]}%")
+        partial_2 = Product.where(category: Category.where('category.name LIKE :q', q: "%#{params[:q]}%"))      
+        @products = partial_1.or(partial_2)
+      else
+        @products = Product.limit(30).offset(0)
+      end
     end
 end
